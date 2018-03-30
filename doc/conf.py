43e138727838563d9f1bb5309352832def07b100
Scott C. Livingston <slivingston@cds.caltech.edu>
2018-03-30 15:49:28 -0700

    DOC: create basic Sphinx HTML configuration

---
+++ doc/conf.py
@@ -0,0 +1,20 @@
+project = 'kuaikai'
+copyright = '2018, kuaikai authors'
+author = 'kuaikai authors'
+
+version = ''
+release = ''
+
+extensions = [
+    'sphinx.ext.mathjax',
+]
+
+source_suffix = '.rst'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+master_doc = 'index'
+
+language = None
+pygments_style = 'sphinx'
+html_theme = 'alabaster'
+html_static_path = ['_static']
+htmlhelp_basename = 'kuaikaidoc'
