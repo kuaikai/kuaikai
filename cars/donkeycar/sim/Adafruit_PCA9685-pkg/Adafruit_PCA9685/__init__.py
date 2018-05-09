0ffaa208f4422a80d90a88d993523cb48358b008
Scott C. Livingston <scott@rerobots.net>
2018-05-09 16:57:20 -0700

    record calls to Adafruit_PCA9685 mock
    Calls to the methods set_pwm_freq() and set_pwm() of the class PCA9685
    are recorded in a mkstemp file with path prefix /tmp/kuaikai_sim_.
    Writes to this file can be monitored from outside the Docker container
    during simulation evaluation, thus providing checks that some
    actuation is attempted.

--- cars/donkeycar/sim/Adafruit_PCA9685-pkg/Adafruit_PCA9685/__init__.py
+++ cars/donkeycar/sim/Adafruit_PCA9685-pkg/Adafruit_PCA9685/__init__.py
@@ -3,14 +3,30 @@
 SCL <scott@rerobots.net>
 2018
 """
+import json
+import os
+import tempfile
+import time
 
 
 class PCA9685:
     def __init__(self):
-        pass
+        self._fd, self._pathname = tempfile.mkstemp(prefix='kuaikai_sim_', dir='/tmp', text=True)
+        self._fp = os.fdopen(self._fd, 'wt')
+
+    def __del__(self):
+        self._fp.close()
 
     def set_pwm_freq(self, freq_hz):
-        pass
+        self._fp.write(json.dumps({
+            'time': time.time(),
+            'fcn': 'set_pwm_freq',
+            'args': {'freq_hz': freq_hz},
+        }) + '\n')
 
     def set_pwm(self, channel, on, off):
-        pass
+        self._fp.write(json.dumps({
+            'time': time.time(),
+            'fcn': 'set_pwm',
+            'args': {'channel': channel, 'on': on, 'off': off},
+        }) + '\n')
