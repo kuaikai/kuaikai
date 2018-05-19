b6158f8921fa0c6884c05f9af3f48a527960a06b
Scott C. Livingston <scott@rerobots.net>
2018-05-18 23:09:14 -0700

    update form of arg to django.urls.path()

--- acarbroker/acarbroker/urls.py
+++ acarbroker/acarbroker/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        path(r'^__debug__/', include(debug_toolbar.urls)),
+        path(r'__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
